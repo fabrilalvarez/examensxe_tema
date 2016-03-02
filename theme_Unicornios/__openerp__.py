87c872a6cc8bb1327f3ce9f82c2b735804f4c6ac
Fran <fabrilalvarez@danielcastelao.org>
2016-03-02 09:38:33 +0100

    Snippet no va Pero el resto perfect

---
+++ theme_Unicornios/__openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Unicornios',
+    'description': 'Este tema te dibuja unicornios por doquier para tu sistema ERP Odoo.',
+    'category': 'Theme/Hidden',
+    'sequence': 1000,
+    'version': '1.0',
+    'author': 'Francisco S.A.',
+    'depends': ['website'],
+    'data': [
+        'views/layout.xml',
+        'views/pages.xml', 
+        'views/assets.xml',
+        'views/snippets.xml',
+    ],
+    'category': 'Theme/Creative',
+    'depends': ['website', 'website_blog', 'sale']
+}
